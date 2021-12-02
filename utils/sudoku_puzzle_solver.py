6fed7853ea401993b64c13d617a9c54fde8f3ac0
abstractigakis@gmail.com <abstractigakis@gmail.com>
2021-12-01 23:12:44 -0500

    checkpoint

--- utils/sudoku_puzzle_solver.py
+++ utils/sudoku_puzzle_solver.py
@@ -14,20 +14,46 @@ class SudokuPuzzleSolver:
         self.puzzle: ndarray = loadtxt(path_to_puzzle, dtype=int)
 
     def __repr__(self) -> str:
-        """the print method
-
-        Returns:
-            str: prints the puzzle
-        """
         return str(self.puzzle)
 
     def __str__(self) -> str:
-        """the str method
+        return str(self.puzzle)
+
+    def is_num_in_row(self, num: int, row: int) -> bool:
+        return num in self.puzzle[row]
+
+    def is_num_in_col(self, num: int, col: int) -> bool:
+        return num in self.puzzle[:, col]
+
+    def get_section_corner(self, row: int, col: int) -> tuple[int, int]:
+        """given a row and column, return the row and column of the corner of the section of the section of the
+        for example (5, 5) -> (3, 3) and  (5, 3) -> (3, 3)
+        """
+        return row - row % 3, col - col % 3
+
+    def get_section(self, row: int, col: int) -> ndarray:
+        """Gets the 3x3 ndarray given of the current square
 
         Returns:
-            str: returns string of the puzzle
+            ndarray: 3x3 ndarray
         """
-        return str(self.puzzle)
+        x, y = self.get_section_corner(row, col)
+        return self.puzzle[x:x+3, y:y+3]
+
+    def is_num_in_section(self, num: int, row: int, col: int) -> bool:
+        return num in self.get_section_corner(row, col)
+
+    def is_safe_to_insert(self, num: int, row: int, col: int) -> bool:
+        return not self.is_num_in_row(num, row) \
+            and not self.is_num_in_col(num, col) \
+            and not self.is_num_in_section(num, row, col)
+
+    def get_first_blank(self):
+        for i in range(len(self.puzzle)):
+            for j in range(len(self.puzzle[i])):
+                if self.puzzle[i][j] == 0:
+                    return i, j
+        return False
 
 
 if __name__ == "__main__":
