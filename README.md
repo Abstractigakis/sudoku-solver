4f459a70a1de9d6af0dd0f09aceb98683275e7bb
abstractigakis@gmail.com <abstractigakis@gmail.com>
2021-12-05 11:48:18 -0500

    thoughts

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # sudoku-solver
 
+## Problem Statement
+
+For the rules of sudoku, google it, and play some games!
+
+The objective is to create an algorithm that can effeciently solve any valid sudoku puzzle.
+
+To complete this, we will use the backtracking algorithm, a bruteforce depth-first-search.
+
+## Defenitions
+
 A sudoku problem can be represented by a $9$ x $9$ tensor called $X$ where $X_{i,j}$ is an integer between $0$ and $9$. $X_{i,j} = 0$ is the case where the tile is empty.
 
 To keep track of where we can add numbers, we will create a $9$ x $9$ x $9$ boolean tensor called $D$.  This is to be thought of as the domains of possibilites for each $X_{i,j}$, where $D_{i,j,k}$ is $1$ if it is possible to insert $k$ intp $X_{i,j}$ without breaking the rules of Sudoku, and $0$ otherwise.
@@ -8,4 +18,16 @@ Some facts to realize is that if we know a number on a tile conclusively, we can
 
 $$X_{i,j} \neq 0 \iff D_{i,j,k} = 0 \ \forall k$$
 
-For more information on the rules of sudoku, google it, and play some games!
+Also Notice that given a gamestate $X$ we can uniquley compute the Domains $D$
+
+## Dealing with concurrency
+
+With all of the concurrency requiremnets, it seems clear that functional programming is better suited.  We will need a better formulation of the process in place.
+
+Consider that we have $n$ Sudoku Puzzles we want to solve at the same time via back tracking.  Call these $P_{1}$ ... $P_{n}$. We should think of these as completly isolated processes, as each puzzle does not any about the other.  
+
+To solve each puzzle $P$ we run the back tracking algorithm, but we want to do this concurrently as well, i.e., for a given puzzle state $X$, there are a maximum $9^3$ options to attempt.  Call each possible Attempt for a given gamestate $A_{X_{i,j,k}}$: i.e. for the current puzzle gamestate $X$, the attempt where we place the number $k$ in tile $i,j$.  This produces a new game state to work on recursively.
+
+Notice again, we want to leverage concurrency here.  I.e. if there are $m$ possible values that $k$ can take, we want try them all in parellel.  In theory this sounds great, but it may lead to combinatorial explosion.
+
+On the first iteration, we made a class `SudokuSolver` that took a puzzle path in the constructor, and stroed in memory.  This did not end up working, because we dont want the `SudokuSolver` to be limited to 1 puzzle it can solve.  In thoery, the `SudokuSolver`.  In theory, we want to instantiate one solver, and have it be able to work on multiple puzzles.  To do this, we create the `load_puzzle` static method to load puzzles in from `.sd` files, and then pass them as arguments, adopting a more functional approach.  This begs the quiestion, why do we need
--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -1,75 +1,100 @@
-from numpy import loadtxt, ndarray, vectorize, void, where, fromfunction
+from numpy import int8, loadtxt, ndarray, vectorize, void, where, fromfunction
 from typing import Union
 
 
 class SudokuSolver:
     """Loads a numpy representation of a sudoku puzzle, and has methods that can be solve via the backtrace A algorithm with some optimizations
+
+    mostly a name space with static methods, but also some variable state for what
+    would otherwise be global couters
     """
 
-    def __init__(self, path_to_puzzle: str) -> None:
+    @staticmethod
+    def load_puzzle(path_to_puzzle) -> ndarray:
         """initalizes object instacne with a filepath to sudoku problem
 
         Args:
             path_to_puzzle (str): this file type must be .sd
+
+        Returns:
+            ndarray: 9x9 sudoku puzzlepuzzle
         """
-        self.puzzle: ndarray = loadtxt(path_to_puzzle, dtype=int)
-        self.inital_puzzle: ndarray = self.puzzle
-        self.domains: ndarray = self.get_domains()
+        return loadtxt(path_to_puzzle, dtype=int)
+
+    def __init__(self) -> None:
+        """initalizes object variabels that are needed.  Mostly counters that would have to be lobal otherwise.
+
+        Args:
+            path_to_puzzle (str): this file type must be .sd
+        """
+        self.naive_back_tracking_attempt_counter: int = 10000
 
     def __repr__(self) -> str:
         return str(self.puzzle)
 
     def __str__(self) -> str:
-        return str(self.puzzle)
+        return f"""
+        naive_back_tracking_attempt_counter {self.naive_back_tracking_attempt_counter}
+        """
 
-    def is_num_in_row(self, num: int, row: int) -> bool:
-        return num in self.puzzle[row]
+    @staticmethod
+    def is_num_in_row(puzzle: ndarray, num: int, row: int) -> bool:
+        return num in puzzle[row]
 
-    def is_num_in_col(self, num: int, col: int) -> bool:
-        return num in self.puzzle[:, col]
+    @staticmethod
+    def is_num_in_col(puzzle: ndarray, num: int, col: int) -> bool:
+        return num in puzzle[:, col]
 
-    def get_section_corner(self, row: int, col: int) -> tuple[int, int]:
+    @staticmethod
+    def get_section_corner(row: int, col: int) -> tuple[int, int]:
         """given a row and column, return the row and column of the corner of the section of the section of the
         for example (5, 5) -> (3, 3) and  (5, 3) -> (3, 3)
         """
         return row - row % 3, col - col % 3
 
-    def get_section(self, row: int, col: int) -> ndarray:
+    @staticmethod
+    def get_section(puzzle: ndarray, row: int, col: int) -> ndarray:
         """Gets the 3x3 ndarray given of the current square
 
         Returns:
             ndarray: 3x3 ndarray
         """
-        x, y = self.get_section_corner(row, col)
-        return self.puzzle[x:x+3, y:y+3]
+        x, y = __class__.get_section_corner(row, col)
+        return puzzle[x:x+3, y:y+3]
 
-    def is_num_in_section(self, num: int, row: int, col: int) -> bool:
-        return num in self.get_section(row, col)
+    @staticmethod
+    def is_num_in_section(puzzle: ndarray, num: int, row: int, col: int) -> bool:
+        return num in __class__.get_section(puzzle, row, col)
 
-    def is_safe_to_insert(self, num: int, row: int, col: int) -> bool:
-        return not self.is_num_in_row(num, row) and not self.is_num_in_col(num, col) and not self.is_num_in_section(num, row, col)
+    @staticmethod
+    def is_safe_to_insert(puzzle: ndarray, num: int, row: int, col: int) -> bool:
+        return not __class__.is_num_in_row(puzzle, num, row) \
+            and not __class__.is_num_in_col(puzzle, num, col) \
+            and not __class__.is_num_in_section(puzzle, num, row, col)
 
-    def get_all_indicies_that_are_num(self, num: int) -> tuple:
+    @staticmethod
+    def get_all_indicies_that_are_num(puzzle: ndarray, num: int) -> tuple:
         """gets all the indicies that are num int the puzzle
 
         Returns:
             tuple: for an index i, result[0][i], result[1][i] represents the x, y pair of a num
         """
-        return where(self.puzzle == num)
+        return where(puzzle == num)
 
-    def get_first_blank(self) -> Union[tuple[int, int], bool]:
+    @staticmethod
+    def get_first_blank(puzzle: ndarray) -> Union[tuple[int, int], bool]:
         """returns x, y coordinates of first blank entry in puzzle or false if there are none
 
         Returns:
             Union[tuple[int, int], bool]: x, y coordinates of first blank entry in puzzle or false if there are none
         """
-        blanks = self.get_all_indicies_that_are_num(0)
+        blanks = __class__.get_all_indicies_that_are_num(puzzle, 0)
         if len(blanks[0]) == 0:
             return False
         return blanks[0][0], blanks[1][0]
 
-    @property
-    def domain(self) -> void:
+    @staticmethod
+    def domain(puzzle: ndarray) -> void:
         """set the domain property for the current puzzle state, which is a 3d boolean tensor where
         each i,j,k entry is true, if the number k-1 can be inserted into square i, j in the puzzle
         """
@@ -79,9 +104,24 @@ class SudokuSolver:
         #  to handle the full tensors.  We also dont want to iterate beacuse
         #  its slow.  So use vecttorize
 
-        vfunc = vectorize(self.is_safe_to_insert)
+        vfunc = vectorize(__class__.is_safe_to_insert)
         return fromfunction(
-            lambda i, j, k: vfunc(num=k+1, row=i, col=j), (9, 9, 9), dtype=int)
+            lambda i, j, k: vfunc(puzzle, num=k+1, row=i, col=j), (9, 9, 9), dtype=int)
+
+    def naive_back_tracking_attempt(self, puzzle: ndarray):
+        self.naive_back_tracking_attempt_counter -= 1
+        if self.naive_back_tracking_attempt_counter <= 0:
+            # give up, because the naive way is too long
+            self.naive_back_tracking_attempt_counter = None
+            return False
+
+        first_blank_tile = self.get_first_blank(puzzle)
+        if(not var):
+            return True
+
+    def naive_back_tracking(self, max_retries=10000):
+        self.naive_back_tracking_attempt_counter = max_retries
+        return self.naive_back_tracking_attempt(self.inital_puzzle)
 
 
 if __name__ == "__main__":
