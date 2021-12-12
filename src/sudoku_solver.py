ffc35e61dbad9401e71706a9d6481b6e1b764ddd
abstractigakis <abstractigakis@gmail.com>
2021-12-12 14:31:47 -0500

    push from abstractigakis

--- src/sudoku_solver.py
+++ src/sudoku_solver.py
@@ -81,6 +81,73 @@ class SudokuSolver:
     #######################################################################################################
     # calculations
     #######################################################################################################
+    @staticmethod
+    def calc_D_ll(X: ndarray) -> ndarray:
+        """set the domain property for the current puzzle state, which is a 3d boolean tensor where
+        each i,j,k entry is true, if the number k-1 can be inserted into square i, j in the puzzle
+        """
+
+        # DEV NOTE: numpy's from function creates 3, 3d tensors i, j, k. the
+        #  is_safe_to_insert method, is only meant to work for one entry, not
+        #  to handle the full tensors.  We also dont want to iterate beacuse
+        #  its slow.  So use vecttorize.
+        #
+        #  NOTE we dont want to vectorize the puzzle.
+
+        # this can be called on each game state on creation, but this is an
+        # expensive operation.  better to only use domains ad-hoc
+
+        vfunc = vectorize(__class__.is_safe_to_insert, excluded={"X"})
+        return fromfunction(
+            lambda i, j, k: vfunc(X, num=k+1, row=i, col=j), (9, 9, 9), dtype=int)
+
+    @staticmethod
+    def calc_D(X: ndarray) -> ndarray:
+        D = []
+        for i in range(9):
+            a = []
+            for j in range(9):
+                b = []
+                for k in range(9):
+                    b.append(__class__.is_safe_to_insert(X, k+1, i, j))
+                a.append(b)
+            D.append(a)
+        return array(D)
+
+    @staticmethod
+    def forward_checking(X: ndarray, D: ndarray, row: int, col: int, num: int, tf) -> ndarray:
+        """on insertion of a num into X at row col, how do the domains change?"""
+        for i in range(9):
+            if (X[i][col] == 0):
+                D[i][col][num-1] = tf
+            if (X[row][i] == 0):
+                D[row][i][num-1] = tf
+        for i in range(3):
+            a = row - row % 3 + i
+            for j in range(3):
+                b = col - col % 3 + j
+                if(X[a][b] == 0):
+                    D[a][b][num-1] = tf
+        return D
+
+    @staticmethod
+    def insert(X: ndarray, D: ndarray, num: int, row: int, col: int) -> ndarray:
+        """Modifies the X, and D ndarrays, i.e. pass by reference"""
+        if not __class__.is_safe_to_insert(X, num, row, col):
+            raise InvalidInsert
+        X[row][col] = num
+        D = __class__.forward_checking(X, D, num, row, col, True)
+        return X, D
+
+    @staticmethod
+    def remove(X: ndarray, D: ndarray, row: int, col: int) -> ndarray:
+        """Modifies the X, and D ndarrays, i.e. pass by reference"""
+        num = X[row][col]
+        if num == 0:
+            raise CantRemoveFromBlank
+        X[row][col] = 0
+        D = __class__.forward_checking(X, D, num, row, col, False)
+        return X, D
 
     #######################################################################################################
     # naive backtracking
@@ -127,6 +194,65 @@ class SudokuSolver:
     #     # for the puzzle, so need to raise puzzle unsolved
     #     raise PuzzleUnsolvable
 
-    # def naive_back_tracking(self, max_attempts: int = 10000) -> tuple[ndarray, int]:
-    #     self.attempt = 0
-    #     return self.naive_back_tracking_attempt(self.X_0, max_attempts), self.attempt
+    def naive_back_tracking(self, max_attempts: int = 10000) -> tuple[ndarray, int]:
+        self.attempt = 0
+        return self.naive_back_tracking_attempt(self.X_0, max_attempts), self.attempt
+
+    # #######################################################################################################
+    # # backtracking with forward checking
+    # #######################################################################################################
+    # def reset_back_tracking_with_forward_checking_attempt_number(self):
+    #     self.back_tracking_with_forward_checking_attempt_number = 0
+
+    # def increment_back_tracking_with_forward_checking_attempt_number(self, max_attempts: int) -> None:
+    #     self.back_tracking_with_forward_checking_attempt_number += 1
+    #     if self.back_tracking_with_forward_checking_attempt_number == max_attempts:
+    #         raise MaxAtteptsReached(max_attempts)
+
+    # def back_tracking_with_forward_checking_attempt(self, game_state: SudokuGameState, max_attempts: int):
+    #     self.increment_back_tracking_with_forward_checking_attempt_number(
+    #         max_attempts)
+
+    #     # will raise NoBlanks if puzzle is solved!
+    #     try:
+    #         row, col = game_state.get_first_blank()
+    #     except NoBlanks:
+    #         # by design, if we give puzzle from the state that is valid,
+    #         # and we reach this section of the code, then we know the puzzle
+    #         # is solved, but really, we should check if the puzzle is valid
+    #         return game_state
+
+    #     for num in range(1, 10):
+    #         # the case where next_puzzle is computed, meaning that it is
+    #         # possible to insert k into X[i][j], so we should try this
+    #         # and recursivly call backtracking on it
+    #         try:
+    #             next_game_state = game_state.next_game_state(num, row, col)
+    #             if next_game_state.is_impossible:
+    #                 reuslt_game_state = self.back_tracking_with_forward_checking_attempt(
+    #                     next_game_state, max_attempts)
+    #                 if isinstance(reuslt_game_state, SudokuGameState):
+    #                     return reuslt_game_state
+
+    #         except NoBlanks:
+    #             # by design, if we give puzzle from the state that is valid,
+    #             # and we reach this section of the code, then we know the puzzle
+    #             # is solved, but really, we should check if the puzzle is valid
+    #             return game_state
+
+    #         except InvalidSudokuGameState:
+    #             # if the result puzzle is in valid, move on and try num+1
+    #             pass
+    #         except PuzzleUnsolvable:
+    #             # if our current puzzle is unsolvable, but we have more to
+    #             # numbers to chekc continue
+    #             pass
+
+    #     # we checked all the numbers for a given spot, and they did not fit
+    #     # for the puzzle, so need to raise puzzle unsolved
+    #     raise PuzzleUnsolvable
+
+    # def back_tracking_with_forward_checking(self, max_attempts: int = 10000) -> tuple[SudokuGameState, int]:
+    #     self.reset_back_tracking_with_forward_checking_attempt_number()
+    #     return self.back_tracking_with_forward_checking_attempt(
+    #         self.intial_game_state, max_attempts), self.back_tracking_with_forward_checking_attempt_number
